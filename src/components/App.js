38a8b2cc7b4b83369d451c05be6ab61825d7ff40
Brett Macy <bam03584@aol.com>
2016-09-16 00:03:12 -0400

    added in transition, button click event and working on header

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
+import Header from './Header'
 import '../styles/screen.sass'
 
 class App extends Component {
-
+  startApp = (event) => {
+    console.log(event.target.parentNode)
+    event.target.parentNode.className += ' hidden'
+  }
   render () {
     return <div className='app'>
+      <Header />
       <div className='modal welcome'>
         <h2>Welcome to</h2>
         <h1>FREE TIME</h1>
-        <p>The App that tells YOU what to do!</p><br /><br />
-        <div className='go'>FREE UP YOUR TIME</div>
+        <p>The App that tells <span>YOU</span> what to <span>DO</span></p><br /><br />
+        <div className='go' onClick={this.startApp}>FREE UP YOUR TIME</div>
       </div>
     </div>
   }
